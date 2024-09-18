4e3f4db6254c5f28f3446d368f4977e09506f716
pri-3x <priyanshu.21march@gmail.com>
2024-09-18 23:34:30 +0530

    Add new resume

--- app.js
+++ app.js
@@ -176,7 +176,7 @@ function hireMe() {
     toggleNavBar();
 }
 function downloadCV() {
-    window.open("Priyanshu_Rajput_Resume.pdf");
+    window.open("Priyanshu__Resume.pdf");
 }
 function validateEmail(input) {
     var emailValidRegex = /^([a-zA-Z\d])([a-zA-Z\d\.]+([\-_+]{1})?)+@([a-zA-Z\d-]+)\.([a-z]{2,10})(\.[a-z]{2,10})?$/;
